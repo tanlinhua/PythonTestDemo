3b2e2051b5d793d6da969b58b8c9daa50275f109
tanlinhua <493383500@qq.com>
2021-08-27 20:35:13 +0800

    add for socket

---
+++ socket/http-server.py
@@ -0,0 +1,40 @@
+import socket
+import threading
+
+WEBROOT = "webroot"
+
+
+def handle_client(c, addr):
+    print(addr, "connected.")
+
+    with c:
+        request = c.recv(1024)
+
+        # Parse HTTP headers
+        headers = request.split(b"\r\n")
+        file = headers[0].split()[1].decode()
+
+        # Load file content
+        if file == "/":
+            file = "/index.html"
+
+        try:
+            with open(WEBROOT+file, "rb") as f:
+                content = f.read()
+            response = b"HTTP/1.0 200 OK \r\n\r\n"+content
+
+        except FileNotFoundError:
+            response = b"HTTP/1.0 200 OK \r\n\r\nFile not found!"
+
+        # Send HTTP response
+        c.sendall(response)
+
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind(("0.0.0.0", 1234))
+    s.listen()
+
+    while True:
+        c, addr = s.accept()
+        t = threading.Thread(target=handle_client, args=(c, addr))
+        t.start()
